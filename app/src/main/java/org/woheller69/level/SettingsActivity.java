9c565444190f44cc9b8c247b83820f08cb467c64
DawnShaker <danila2002kukil@gmail.com>
2022-07-10 13:15:31 +0300

    first commit

---
+++ app/src/main/java/org/woheller69/level/SettingsActivity.java
@@ -0,0 +1,20 @@
+package org.woheller69.level;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings_container, new SettingsFragment())
+                    .commit();
+        }
+    }
+}
